b7d0e1f44899b78eb82f87387897233cfb068534
gyeongsu <choiiisu11@gmail.com>
2024-05-16 12:43:58 +0900

    feat: AuthorizationFilter 구현
    사용자 인가를 위한 AuthorizationFilter 구현

---
+++ BE/src/main/java/com/moshimoshi/filter/AuthorizationFilter.java
@@ -0,0 +1,68 @@
+package com.moshimoshi.filter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moshimoshi.auth.utils.JwtProvider;
+import com.moshimoshi.common.Define;
+import com.moshimoshi.common.exception.CommonException;
+import com.moshimoshi.common.exception.ErrorCode;
+import com.moshimoshi.user.domain.Role;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuthorizationFilter implements Filter {
+    private final String[] allowedURIs = new String[] {"/api/users/login", "/api/auth/refresh/token", "/api/users"};
+    private final JwtProvider jwtProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if (isAllowedUris(httpServletRequest.getRequestURI())) {
+            chain.doFilter(request, response);
+            return;
+        }
+        if (!isTokenExist(httpServletRequest)) {
+            throw new CommonException(ErrorCode.ACCESS_TOKEN_NOT_EXIST);
+        }
+        String token = getToken(httpServletRequest);
+        AuthenticatedUser authenticatedUser = getAuthenticatedUser(token);
+        authorize(httpServletRequest.getRequestURI(), authenticatedUser);
+        log.info("[{}] logged in", authenticatedUser.getLoginId());
+        chain.doFilter(request, response);
+    }
+
+    private boolean isAllowedUris(String requestURI) {
+        return PatternMatchUtils.simpleMatch(allowedURIs, requestURI);
+    }
+
+    private boolean isTokenExist(HttpServletRequest httpServletRequest) {
+        String authorization = httpServletRequest.getHeader("Authorization");
+        return authorization != null && authorization.startsWith("Bearer ");
+    }
+
+    private String getToken(HttpServletRequest httpServletRequest){
+        String authorization = httpServletRequest.getHeader("Authorization");
+        return authorization.substring(7);
+    }
+
+    private AuthenticatedUser getAuthenticatedUser(String token) throws JsonProcessingException {
+        Claims claims = jwtProvider.getClaims(token);
+        String authenticatedUserJson = claims.get(Define.AUTHENTICATED, String.class);
+        return objectMapper.readValue(authenticatedUserJson, AuthenticatedUser.class);
+    }
+
+    private void authorize(String requestURI, AuthenticatedUser authenticatedUser) {
+        if (PatternMatchUtils.simpleMatch("*/admin*", requestURI) && !Role.ADMIN.equals(authenticatedUser.getRole())){
+            throw new CommonException(ErrorCode.FORBIDDEN);
+        }
+    }
+}
